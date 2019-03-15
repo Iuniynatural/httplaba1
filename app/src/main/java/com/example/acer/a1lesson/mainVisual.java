bce10a43769b7173807b6853e9f2ab83d2e54abb
Iuniynatural <suseka.yalublu@yandex.ru>
2019-03-16 01:40:03 +0500

    update 1

---
+++ app/src/main/java/com/example/acer/a1lesson/mainVisual.java
@@ -0,0 +1,13 @@
+package com.example.acer.a1lesson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainVisual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_visual);
+    }
+}
